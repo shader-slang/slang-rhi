21a9c5f0ca87a7d11cc4a7a6a26f335d4c4aee15
Simon Kallweit <skallweit@nvidia.com>
2024-08-26 14:34:49 +0200

    implement shared library loading

--- src/utils/platform.cpp
+++ src/utils/platform.cpp
@@ -2,24 +2,57 @@
 
 #include "assert.h"
 
+#if SLANG_WINDOWS_FAMILY
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+#elif SLANG_LINUX_FAMILY
+#include <dlfcn.h>
+#else
+#error "Unsupported platform"
+#endif
+
 namespace gfx {
 
 SlangResult loadSharedLibrary(const char* path, SharedLibraryHandle& handleOut)
 {
+#if SLANG_WINDOWS_FAMILY
+    handleOut = LoadLibraryA(path);
+    if (!handleOut)
+    {
+        return SLANG_FAIL;
+    }
+    return SLANG_OK;
+#elif SLANG_LINUX_FAMILY
+    handleOut = dlopen(path, RTLD_LAZY);
+    if (!handleOut)
+    {
+        return SLANG_FAIL;
+    }
+#else
     SLANG_RHI_ASSERT_FAILURE("Not implemented");
+#endif
 }
 
 void unloadSharedLibrary(SharedLibraryHandle handle)
 {
+#if SLANG_WINDOWS_FAMILY
+    FreeLibrary(static_cast<HMODULE>(handle));
+#elif SLANG_LINUX_FAMILY
+    dlclose(handle);
+#else
     SLANG_RHI_ASSERT_FAILURE("Not implemented");
+#endif
 }
 
-/// Given a shared library handle and a name, return the associated object
-/// Return nullptr if object is not found
-/// @param The shared library handle as returned by loadPlatformLibrary
 void* findSymbolAddressByName(SharedLibraryHandle handle, char const* name)
 {
+#if SLANG_WINDOWS_FAMILY
+    return GetProcAddress(static_cast<HMODULE>(handle), name);
+#elif SLANG_LINUX_FAMILY
+    return dlsym(handle, name);
+#else
     SLANG_RHI_ASSERT_FAILURE("Not implemented");
+#endif
 }
 
 } // namespace gfx
