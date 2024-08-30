e85923ad543400e96dd89989c8748b70038b28e8
Simon Kallweit <skallweit@nvidia.com>
2024-08-30 13:55:15 +0200

    improve assert handling

--- src/core/assert.h
+++ src/core/assert.h
@@ -1,24 +1,28 @@
 #pragma once
 
-#include <cstdlib>
+namespace rhi {
+
+[[noreturn]] void handleAssert(const char* message, const char* file, int line);
+
+}
+
+#define SLANG_RHI_ASSERT_FAILURE(what)                                                                                 \
+    {                                                                                                                  \
+        ::rhi::handleAssert(what, __FILE__, __LINE__);                                                                 \
+    }
 
 #define SLANG_RHI_ASSERT(x)                                                                                            \
     {                                                                                                                  \
         if (!(x))                                                                                                      \
-            std::abort();                                                                                              \
-    }
-
-#define SLANG_RHI_ASSERT_FAILURE(what)                                                                                 \
-    {                                                                                                                  \
-        std::abort();                                                                                                  \
+            SLANG_RHI_ASSERT_FAILURE(#x);                                                                              \
     }
 
 #define SLANG_RHI_UNIMPLEMENTED_X(what)                                                                                \
     {                                                                                                                  \
-        std::abort();                                                                                                  \
+        SLANG_RHI_ASSERT_FAILURE("Not implemented");                                                                   \
     }
 
 #define SLANG_RHI_UNREACHABLE(what)                                                                                    \
     {                                                                                                                  \
-        std::abort();                                                                                                  \
+        SLANG_RHI_ASSERT_FAILURE("Unreachable");                                                                       \
     }
