2051c45656ea3e5b4c796c6fae2fa6ad9fe1fb32
Simon Kallweit <skallweit@nvidia.com>
2024-09-23 21:29:45 +0200

    add enum to string conversions

---
+++ src/enum-strings.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <slang-rhi.h>
+
+#include <string>
+
+namespace rhi {
+
+const char* enumToString(DeviceType value);
+const char* enumToString(Format value);
+const char* enumToString(FormatSupport value);
+const char* enumToString(MemoryType value);
+const char* enumToString(BufferUsage value);
+std::string flagsToString(BufferUsage value);
+const char* enumToString(TextureType value);
+const char* enumToString(TextureUsage value);
+std::string flagsToString(TextureUsage value);
+const char* enumToString(ResourceState value);
+const char* enumToString(TextureFilteringMode value);
+const char* enumToString(TextureAddressingMode value);
+const char* enumToString(ComparisonFunc value);
+const char* enumToString(TextureReductionOp value);
+const char* enumToString(InputSlotClass value);
+const char* enumToString(PrimitiveType value);
+
+} // namespace rhi
