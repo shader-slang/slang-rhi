1bd6bc3162409d26008856df55c30eb58a192c8b
Simon Kallweit <skallweit@nvidia.com>
2024-10-03 16:36:30 +0200

    example fix

--- include/slang-rhi/glfw.h
+++ include/slang-rhi/glfw.h
@@ -7,6 +7,9 @@ WindowHandle getWindowHandleFromGLFW(GLFWwindow* window)
 #if SLANG_WINDOWS_FAMILY
     HWND hwnd = glfwGetWin32Window(window);
     return WindowHandle::FromHwnd(hwnd);
+#elif SLANG_LINUX_FAMILY
+    Window xwindow = glfwGetX11Window(window);
+    return WindowHandle::FromXWindow(0, xwindow);
 #elif SLANG_APPLE_FAMILY
     id nswindow = glfwGetCocoaWindow(window);
     return WindowHandle::FromNSWindow(nswindow);
