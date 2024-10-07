d59c395747454770e891b8a415e63d76b7c99844
Simon Kallweit <skallweit@nvidia.com>
2024-10-07 14:54:28 +0200

    fix glfw window handle utility

--- include/slang-rhi/glfw.h
+++ include/slang-rhi/glfw.h
@@ -1,21 +1,22 @@
+#pragma once
+
 #include <slang-rhi.h>
 
 namespace rhi {
 
-WindowHandle getWindowHandleFromGLFW(GLFWwindow* window)
+inline WindowHandle getWindowHandleFromGLFW(GLFWwindow* window)
 {
-#if SLANG_WINDOWS_FAMILY
+#if SLANG_WINDOWS_FAMILY && defined(GLFW_EXPOSE_NATIVE_WIN32)
     HWND hwnd = glfwGetWin32Window(window);
     return WindowHandle::FromHwnd(hwnd);
-#elif SLANG_LINUX_FAMILY
+#elif SLANG_LINUX_FAMILY && defined(GLFW_EXPOSE_NATIVE_X11)
     Window xwindow = glfwGetX11Window(window);
     return WindowHandle::FromXWindow(0, xwindow);
-#elif SLANG_APPLE_FAMILY
+#elif SLANG_APPLE_FAMILY && defined(GLFW_EXPOSE_NATIVE_COCOA)
     id nswindow = glfwGetCocoaWindow(window);
     return WindowHandle::FromNSWindow(nswindow);
-#else
-#error "Unsupported platform!"
 #endif
+    return WindowHandle{WindowHandle::Type::Unknown};
 }
 
 } // namespace rhi
