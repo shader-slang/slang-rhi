468797541cbe72fb971119fd30cbc062cd92606f
Simon Kallweit <skallweit@nvidia.com>
2024-10-07 19:50:01 +0200

    surface validation

--- src/debug-layer/debug-surface.cpp
+++ src/debug-layer/debug-surface.cpp
@@ -6,21 +6,49 @@ namespace rhi::debug {
 
 const SurfaceInfo& DebugSurface::getInfo()
 {
+    SLANG_RHI_API_FUNC;
+
     return baseObject->getInfo();
 }
 
 const SurfaceConfig& DebugSurface::getConfig()
 {
+    SLANG_RHI_API_FUNC;
+
     return baseObject->getConfig();
 }
 
 Result DebugSurface::configure(const SurfaceConfig& config)
 {
-    return baseObject->configure(config);
+    SLANG_RHI_API_FUNC;
+
+    m_configured = false;
+
+    if (config.format == Format::Unknown)
+    {
+        RHI_VALIDATION_INFO("Configuring with unknown surface format, choosing the preferred format.");
+    }
+    if (config.width == 0 || config.height == 0)
+    {
+        RHI_VALIDATION_ERROR("Configuring with zero width or height.");
+        return SLANG_FAIL;
+    }
+
+    Result result = baseObject->configure(config);
+    m_configured = SLANG_SUCCEEDED(result);
+    return result;
 }
 
 Result DebugSurface::getCurrentTexture(ITexture** outTexture)
 {
+    SLANG_RHI_API_FUNC;
+
+    if (!m_configured)
+    {
+        RHI_VALIDATION_ERROR("Surface is not configured.");
+        return SLANG_FAIL;
+    }
+
     RefPtr<DebugTexture> texture = new DebugTexture();
     SLANG_RETURN_ON_FAIL(baseObject->getCurrentTexture(texture->baseObject.writeRef()));
     returnComPtr(outTexture, texture);
@@ -29,6 +57,14 @@ Result DebugSurface::getCurrentTexture(ITexture** outTexture)
 
 Result DebugSurface::present()
 {
+    SLANG_RHI_API_FUNC;
+
+    if (!m_configured)
+    {
+        RHI_VALIDATION_ERROR("Surface is not configured.");
+        return SLANG_FAIL;
+    }
+
     return baseObject->present();
 }
 
