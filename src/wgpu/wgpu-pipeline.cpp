bb14e1bd01fdb0dcdd5bdd14fbdf3266c2e452f1
Simon Kallweit <skallweit@nvidia.com>
2024-10-10 15:35:49 +0200

    wgpu backend

--- src/wgpu/wgpu-pipeline.cpp
+++ src/wgpu/wgpu-pipeline.cpp
@@ -7,46 +7,25 @@
 
 namespace rhi::wgpu {
 
-PipelineImpl::~PipelineImpl()
+RenderPipelineImpl::~RenderPipelineImpl()
 {
     if (m_renderPipeline)
     {
         m_device->m_ctx.api.wgpuRenderPipelineRelease(m_renderPipeline);
     }
-    if (m_computePipeline)
-    {
-        m_device->m_ctx.api.wgpuComputePipelineRelease(m_computePipeline);
-    }
 }
 
-void PipelineImpl::init(const RenderPipelineDesc& desc)
+Result RenderPipelineImpl::getNativeHandle(NativeHandle* outHandle)
 {
-    PipelineStateDesc pipelineDesc;
-    pipelineDesc.type = PipelineType::Graphics;
-    pipelineDesc.graphics = desc;
-    initializeBase(pipelineDesc);
+    outHandle->type = NativeHandleType::WGPURenderPipeline;
+    outHandle->value = (uint64_t)m_renderPipeline;
+    return SLANG_OK;
 }
 
-void PipelineImpl::init(const ComputePipelineDesc& desc)
+Result DeviceImpl::createRenderPipeline2(const RenderPipelineDesc2& desc, IRenderPipeline** outPipeline)
 {
-    PipelineStateDesc pipelineDesc;
-    pipelineDesc.type = PipelineType::Compute;
-    pipelineDesc.compute = desc;
-    initializeBase(pipelineDesc);
-}
-
-void PipelineImpl::init(const RayTracingPipelineDesc& desc)
-{
-    PipelineStateDesc pipelineDesc;
-    pipelineDesc.type = PipelineType::RayTracing;
-    pipelineDesc.rayTracing = desc;
-    initializeBase(pipelineDesc);
-}
-
-Result PipelineImpl::createRenderPipeline()
-{
-    ShaderProgramImpl* program = checked_cast<ShaderProgramImpl*>(m_program.get());
-    InputLayoutImpl* inputLayout = checked_cast<InputLayoutImpl*>(desc.graphics.inputLayout);
+    ShaderProgramImpl* program = checked_cast<ShaderProgramImpl*>(desc.program);
+    InputLayoutImpl* inputLayout = checked_cast<InputLayoutImpl*>(desc.inputLayout);
     ShaderProgramImpl::Module* vertexModule = program->findModule(SlangStage::SLANG_STAGE_VERTEX);
     ShaderProgramImpl::Module* fragmentModule = program->findModule(SlangStage::SLANG_STAGE_FRAGMENT);
     if (!vertexModule || !fragmentModule)
@@ -63,17 +42,17 @@ Result PipelineImpl::createRenderPipeline()
     pipelineDesc.vertex.buffers = inputLayout->m_vertexBufferLayouts.data();
     pipelineDesc.vertex.bufferCount = (uint32_t)inputLayout->m_vertexBufferLayouts.size();
 
-    pipelineDesc.primitive.topology = translatePrimitiveTopology(desc.graphics.primitiveTopology);
+    pipelineDesc.primitive.topology = translatePrimitiveTopology(desc.primitiveTopology);
     // TODO support strip topologies
     pipelineDesc.primitive.stripIndexFormat = WGPUIndexFormat_Undefined;
-    pipelineDesc.primitive.frontFace = translateFrontFace(desc.graphics.rasterizer.frontFace);
-    pipelineDesc.primitive.cullMode = translateCullMode(desc.graphics.rasterizer.cullMode);
-    pipelineDesc.primitive.unclippedDepth = !desc.graphics.rasterizer.depthClipEnable;
+    pipelineDesc.primitive.frontFace = translateFrontFace(desc.rasterizer.frontFace);
+    pipelineDesc.primitive.cullMode = translateCullMode(desc.rasterizer.cullMode);
+    pipelineDesc.primitive.unclippedDepth = !desc.rasterizer.depthClipEnable;
 
     WGPUDepthStencilState depthStencil = {};
-    if (desc.graphics.depthStencil.format != Format::Unknown)
+    if (desc.depthStencil.format != Format::Unknown)
     {
-        const DepthStencilState& depthStencilIn = desc.graphics.depthStencil;
+        const DepthStencilState& depthStencilIn = desc.depthStencil;
         depthStencil.format = translateTextureFormat(depthStencilIn.format);
         depthStencil.depthWriteEnabled =
             depthStencilIn.depthWriteEnable ? WGPUOptionalBool_True : WGPUOptionalBool_False;
@@ -88,22 +67,22 @@ Result PipelineImpl::createRenderPipeline()
         depthStencil.stencilBack.passOp = translateStencilOp(depthStencilIn.backFace.stencilPassOp);
         depthStencil.stencilReadMask = depthStencilIn.stencilReadMask;
         depthStencil.stencilWriteMask = depthStencilIn.stencilWriteMask;
-        depthStencil.depthBias = desc.graphics.rasterizer.depthBias;
-        depthStencil.depthBiasSlopeScale = desc.graphics.rasterizer.slopeScaledDepthBias;
-        depthStencil.depthBiasClamp = desc.graphics.rasterizer.depthBiasClamp;
+        depthStencil.depthBias = desc.rasterizer.depthBias;
+        depthStencil.depthBiasSlopeScale = desc.rasterizer.slopeScaledDepthBias;
+        depthStencil.depthBiasClamp = desc.rasterizer.depthBiasClamp;
         pipelineDesc.depthStencil = &depthStencil;
     }
 
-    pipelineDesc.multisample.count = desc.graphics.multisample.sampleCount;
-    pipelineDesc.multisample.mask = desc.graphics.multisample.sampleMask;
-    pipelineDesc.multisample.alphaToCoverageEnabled = desc.graphics.multisample.alphaToCoverageEnable;
-    // desc.graphics.multisample.alphaToOneEnable not supported
+    pipelineDesc.multisample.count = desc.multisample.sampleCount;
+    pipelineDesc.multisample.mask = desc.multisample.sampleMask;
+    pipelineDesc.multisample.alphaToCoverageEnabled = desc.multisample.alphaToCoverageEnable;
+    // desc.multisample.alphaToOneEnable not supported
 
-    short_vector<WGPUColorTargetState, 8> targets(desc.graphics.targetCount, {});
-    short_vector<WGPUBlendState, 8> blendStates(desc.graphics.targetCount, {});
-    for (GfxIndex i = 0; i < desc.graphics.targetCount; ++i)
+    short_vector<WGPUColorTargetState, 8> targets(desc.targetCount, {});
+    short_vector<WGPUBlendState, 8> blendStates(desc.targetCount, {});
+    for (GfxIndex i = 0; i < desc.targetCount; ++i)
     {
-        const ColorTargetState& targetIn = desc.graphics.targets[i];
+        const ColorTargetState& targetIn = desc.targets[i];
         WGPUColorTargetState& target = targets[i];
         WGPUBlendState& blend = blendStates[i];
         target.format = translateTextureFormat(targetIn.format);
@@ -127,14 +106,35 @@ Result PipelineImpl::createRenderPipeline()
     fragment.targets = targets.data();
     pipelineDesc.fragment = &fragment;
 
-    m_renderPipeline = m_device->m_ctx.api.wgpuDeviceCreateRenderPipeline(m_device->m_ctx.device, &pipelineDesc);
-
-    return m_renderPipeline ? SLANG_OK : SLANG_FAIL;
+    RefPtr<RenderPipelineImpl> pipeline = new RenderPipelineImpl();
+    pipeline->m_device = this;
+    pipeline->m_renderPipeline = m_ctx.api.wgpuDeviceCreateRenderPipeline(m_ctx.device, &pipelineDesc);
+    if (!pipeline->m_renderPipeline)
+    {
+        return SLANG_FAIL;
+    }
+    returnComPtr(outPipeline, pipeline);
+    return SLANG_OK;
 }
 
-Result PipelineImpl::createComputePipeline()
+ComputePipelineImpl::~ComputePipelineImpl()
 {
-    ShaderProgramImpl* program = checked_cast<ShaderProgramImpl*>(m_program.get());
+    if (m_computePipeline)
+    {
+        m_device->m_ctx.api.wgpuComputePipelineRelease(m_computePipeline);
+    }
+}
+
+Result ComputePipelineImpl::getNativeHandle(NativeHandle* outHandle)
+{
+    outHandle->type = NativeHandleType::WGPUComputePipeline;
+    outHandle->value = (uint64_t)m_computePipeline;
+    return SLANG_OK;
+}
+
+Result DeviceImpl::createComputePipeline2(const ComputePipelineDesc2& desc, IComputePipeline** outPipeline)
+{
+    ShaderProgramImpl* program = checked_cast<ShaderProgramImpl*>(desc.program);
     ShaderProgramImpl::Module* computeModule = program->findModule(SlangStage::SLANG_STAGE_COMPUTE);
     if (!computeModule)
     {
@@ -146,62 +146,18 @@ Result PipelineImpl::createComputePipeline()
     pipelineDesc.compute.module = computeModule->module;
     pipelineDesc.compute.entryPoint = computeModule->entryPointName.c_str();
 
-    m_computePipeline = m_device->m_ctx.api.wgpuDeviceCreateComputePipeline(m_device->m_ctx.device, &pipelineDesc);
-
-    return m_computePipeline ? SLANG_OK : SLANG_FAIL;
-}
-
-Result PipelineImpl::ensureAPIPipelineCreated()
-{
-    switch (desc.type)
+    RefPtr<ComputePipelineImpl> pipeline = new ComputePipelineImpl();
+    pipeline->m_device = this;
+    pipeline->m_computePipeline = m_ctx.api.wgpuDeviceCreateComputePipeline(m_ctx.device, &pipelineDesc);
+    if (!pipeline->m_computePipeline)
     {
-    case PipelineType::Compute:
-        return m_computePipeline ? SLANG_OK : createComputePipeline();
-    case PipelineType::Graphics:
-        return m_renderPipeline ? SLANG_OK : createRenderPipeline();
-    default:
-        SLANG_RHI_UNREACHABLE("Unknown pipeline type.");
         return SLANG_FAIL;
     }
-    return SLANG_OK;
-}
-
-Result PipelineImpl::getNativeHandle(NativeHandle* outHandle)
-{
-    switch (desc.type)
-    {
-    case PipelineType::Compute:
-        outHandle->type = NativeHandleType::WGPUComputePipeline;
-        outHandle->value = (uint64_t)m_computePipeline;
-        return SLANG_OK;
-    case PipelineType::Graphics:
-        outHandle->type = NativeHandleType::WGPURenderPipeline;
-        outHandle->value = (uint64_t)m_renderPipeline;
-        return SLANG_OK;
-    }
-    return SLANG_E_NOT_AVAILABLE;
-}
-
-
-Result DeviceImpl::createRenderPipeline(const RenderPipelineDesc& desc, IPipeline** outPipeline)
-{
-    RefPtr<PipelineImpl> pipeline = new PipelineImpl();
-    pipeline->m_device = this;
-    pipeline->init(desc);
     returnComPtr(outPipeline, pipeline);
     return SLANG_OK;
 }
 
-Result DeviceImpl::createComputePipeline(const ComputePipelineDesc& desc, IPipeline** outPipeline)
-{
-    RefPtr<PipelineImpl> pipeline = new PipelineImpl();
-    pipeline->m_device = this;
-    pipeline->init(desc);
-    returnComPtr(outPipeline, pipeline);
-    return SLANG_OK;
-}
-
-Result DeviceImpl::createRayTracingPipeline(const RayTracingPipelineDesc& desc, IPipeline** outPipeline)
+Result DeviceImpl::createRayTracingPipeline2(const RayTracingPipelineDesc2& desc, IRayTracingPipeline** outPipeline)
 {
     return SLANG_E_NOT_IMPLEMENTED;
 }
