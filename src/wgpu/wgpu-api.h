c5442316b0ccd8b154b3d2c274de312838f23bd4
Simon Kallweit <skallweit@nvidia.com>
2024-11-11 18:28:55 +0100

    do not change auto-generated code

--- src/wgpu/wgpu-api.h
+++ src/wgpu/wgpu-api.h
@@ -197,6 +197,7 @@
     /* Procs of Queue */ \
     x(QueueCopyExternalTextureForBrowser) \
     x(QueueCopyTextureForBrowser) \
+    x(QueueOnSubmittedWorkDone) \
     x(QueueOnSubmittedWorkDone2) \
     x(QueueOnSubmittedWorkDoneF) \
     x(QueueSetLabel) \
