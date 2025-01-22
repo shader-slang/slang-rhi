78e97c402df604bb2145029bae5f8ee4124ac5fb
Simon Kallweit <skallweit@nvidia.com>
2025-01-22 14:05:46 +0100

    formatting

--- src/rhi-shared.cpp
+++ src/rhi-shared.cpp
@@ -679,7 +679,13 @@ void CommandEncoder::clearTexture(
     m_commandList->write(std::move(cmd));
 }
 
-void CommandEncoder::resolveQuery(IQueryPool* queryPool, uint32_t index, uint32_t count, IBuffer* buffer, uint64_t offset)
+void CommandEncoder::resolveQuery(
+    IQueryPool* queryPool,
+    uint32_t index,
+    uint32_t count,
+    IBuffer* buffer,
+    uint64_t offset
+)
 {
     commands::ResolveQuery cmd;
     cmd.queryPool = queryPool;
