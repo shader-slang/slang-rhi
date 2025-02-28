0a27ad0e9a63ab05bcb86cc3ea675155516002a2
ccummingsNV <ccummings@nvidia.com>
2025-02-28 09:51:21 +0000

    Extract shaders (#199)
    * Moved shader object header code
    
    * Extract shader object
    
    * Moved shader headers
    
    * Move shader program c++ side
    
    * Move some cpp code for device that was missed previously
    
    * Moved shader headers
    
    * Move shader program c++ side
    
    * Move some cpp code for device that was missed previously

---
+++ src/shader.cpp
@@ -0,0 +1,168 @@
+#include "shader.h"
+
+#include "rhi-shared.h"
+#include "shader-object.h"
+
+namespace rhi {
+
+// ----------------------------------------------------------------------------
+// SpecializationKey
+// ----------------------------------------------------------------------------
+
+SpecializationKey::SpecializationKey(const ExtendedShaderObjectTypeList& args)
+    : componentIDs(args.componentIDs)
+{
+}
+
+// ----------------------------------------------------------------------------
+// ShaderProgram
+// ----------------------------------------------------------------------------
+
+IShaderProgram* ShaderProgram::getInterface(const Guid& guid)
+{
+    if (guid == ISlangUnknown::getTypeGuid() || guid == IShaderProgram::getTypeGuid())
+        return static_cast<IShaderProgram*>(this);
+    return nullptr;
+}
+
+void ShaderProgram::init(const ShaderProgramDesc& desc)
+{
+    m_desc = desc;
+
+    slangGlobalScope = desc.slangGlobalScope;
+    for (uint32_t i = 0; i < desc.slangEntryPointCount; i++)
+    {
+        slangEntryPoints.push_back(ComPtr<slang::IComponentType>(desc.slangEntryPoints[i]));
+    }
+
+    auto session = desc.slangGlobalScope ? desc.slangGlobalScope->getSession() : nullptr;
+    if (desc.linkingStyle == LinkingStyle::SingleProgram)
+    {
+        std::vector<slang::IComponentType*> components;
+        if (desc.slangGlobalScope)
+        {
+            components.push_back(desc.slangGlobalScope);
+        }
+        for (uint32_t i = 0; i < desc.slangEntryPointCount; i++)
+        {
+            if (!session)
+            {
+                session = desc.slangEntryPoints[i]->getSession();
+            }
+            components.push_back(desc.slangEntryPoints[i]);
+        }
+        session->createCompositeComponentType(components.data(), components.size(), linkedProgram.writeRef());
+    }
+    else
+    {
+        for (uint32_t i = 0; i < desc.slangEntryPointCount; i++)
+        {
+            if (desc.slangGlobalScope)
+            {
+                slang::IComponentType* entryPointComponents[2] = {desc.slangGlobalScope, desc.slangEntryPoints[i]};
+                ComPtr<slang::IComponentType> linkedEntryPoint;
+                session->createCompositeComponentType(entryPointComponents, 2, linkedEntryPoint.writeRef());
+                linkedEntryPoints.push_back(linkedEntryPoint);
+            }
+            else
+            {
+                linkedEntryPoints.push_back(ComPtr<slang::IComponentType>(desc.slangEntryPoints[i]));
+            }
+        }
+        linkedProgram = desc.slangGlobalScope;
+    }
+
+    m_isSpecializable = _isSpecializable();
+}
+
+Result ShaderProgram::compileShaders(Device* device)
+{
+    if (m_compiledShaders)
+        return SLANG_OK;
+
+    if (device->getDeviceInfo().deviceType == DeviceType::CPU)
+    {
+        // CPU device does not need to compile shaders.
+        m_compiledShaders = true;
+        return SLANG_OK;
+    }
+
+    // For a fully specialized program, read and store its kernel code in `shaderProgram`.
+    auto compileShader = [&](slang::EntryPointReflection* entryPointInfo,
+                             slang::IComponentType* entryPointComponent,
+                             SlangInt entryPointIndex)
+    {
+        ComPtr<ISlangBlob> kernelCode;
+        ComPtr<ISlangBlob> diagnostics;
+        auto compileResult = device->getEntryPointCodeFromShaderCache(
+            entryPointComponent,
+            entryPointIndex,
+            0,
+            kernelCode.writeRef(),
+            diagnostics.writeRef()
+        );
+        if (diagnostics)
+        {
+            DebugMessageType msgType = DebugMessageType::Warning;
+            if (compileResult != SLANG_OK)
+                msgType = DebugMessageType::Error;
+            device->handleMessage(msgType, DebugMessageSource::Slang, (char*)diagnostics->getBufferPointer());
+        }
+        SLANG_RETURN_ON_FAIL(compileResult);
+        SLANG_RETURN_ON_FAIL(createShaderModule(entryPointInfo, kernelCode));
+        return SLANG_OK;
+    };
+
+    if (linkedEntryPoints.size() == 0)
+    {
+        // If the user does not explicitly specify entry point components, find them from
+        // `linkedEntryPoints`.
+        auto programReflection = linkedProgram->getLayout();
+        for (SlangUInt i = 0; i < programReflection->getEntryPointCount(); i++)
+        {
+            SLANG_RETURN_ON_FAIL(compileShader(programReflection->getEntryPointByIndex(i), linkedProgram, (SlangInt)i));
+        }
+    }
+    else
+    {
+        // If the user specifies entry point components via the separated entry point array,
+        // compile code from there.
+        for (auto& entryPoint : linkedEntryPoints)
+        {
+            SLANG_RETURN_ON_FAIL(compileShader(entryPoint->getLayout()->getEntryPointByIndex(0), entryPoint, 0));
+        }
+    }
+
+    m_compiledShaders = true;
+
+    return SLANG_OK;
+}
+
+Result ShaderProgram::createShaderModule(slang::EntryPointReflection* entryPointInfo, ComPtr<ISlangBlob> kernelCode)
+{
+    SLANG_UNUSED(entryPointInfo);
+    SLANG_UNUSED(kernelCode);
+    return SLANG_OK;
+}
+
+bool ShaderProgram::isMeshShaderProgram() const
+{
+    // Similar to above, interrogate either explicity specified entry point
+    // componenets or the ones in the linked program entry point array
+    if (linkedEntryPoints.size())
+    {
+        for (const auto& e : linkedEntryPoints)
+            if (e->getLayout()->getEntryPointByIndex(0)->getStage() == SLANG_STAGE_MESH)
+                return true;
+    }
+    else
+    {
+        const auto programReflection = linkedProgram->getLayout();
+        for (SlangUInt i = 0; i < programReflection->getEntryPointCount(); ++i)
+            if (programReflection->getEntryPointByIndex(i)->getStage() == SLANG_STAGE_MESH)
+                return true;
+    }
+    return false;
+}
+
+}
