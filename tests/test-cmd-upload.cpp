c73c667ac0781aa0bfe1bac7e5e67ea5167a98be
Simon Kallweit <64953474+skallweitNV@users.noreply.github.com>
2025-03-10 17:23:52 +0100

    implement CUDA texture upload (#237)
    * implement CUDA texture upload
    
    * always use cuMemcpy3D

--- tests/test-cmd-upload.cpp
+++ tests/test-cmd-upload.cpp
@@ -131,7 +131,7 @@ GPU_TEST_CASE("cmd-upload-buffer-multienc", ALL)
 using namespace rhi;
 using namespace rhi::testing;
 
-GPU_TEST_CASE("cmd-upload-texture-2d-nomip", D3D12 | Vulkan | WGPU)
+GPU_TEST_CASE("cmd-upload-texture-2d-nomip", D3D12 | Vulkan | WGPU | CUDA)
 {
     TextureDesc textureDesc = {};
     textureDesc.type = TextureType::Texture2D;
