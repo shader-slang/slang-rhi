cdde07749bfabcee5c8a798708a051906703e432
Chris Cummings <ccummings@nvidia.com>
2025-03-11 16:49:03 +0000

    Restrict create test to D3D12 until fully working

--- tests/test-create-texture.cpp
+++ tests/test-create-texture.cpp
@@ -7,7 +7,7 @@
 using namespace rhi;
 using namespace rhi::testing;
 
-GPU_TEST_CASE("texturetest-create", ALL & ~CUDA)
+GPU_TEST_CASE("texturetest-create", D3D12)
 {
     TextureTestOptions options(device, 1);
     options.addVariants(TTShape::All, TTArray::Both, TTMip::Both, TTMS::Both);
