361333408ddc48b667fd281af85d620c81bc86e9
Chris Cummings <ccummings@nvidia.com>
2025-03-12 13:39:19 +0000

    D3D11 texture read works

--- tests/test-create-texture.cpp
+++ tests/test-create-texture.cpp
@@ -9,7 +9,7 @@ using namespace rhi::testing;
 
 GPU_TEST_CASE("texturetest-create", ALL)
 {
-    TextureTestOptions options(device, 1);
+    TextureTestOptions options(device, TextureInitMode::Random);
     options.addVariants(TTShape::All, TTArray::Both, TTMip::Both, TTMS::Both);
 
 
