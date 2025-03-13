03ebe6ec9f3c34147602b354b5c232369fadbfbc
Chris Cummings <ccummings@nvidia.com>
2025-03-13 12:36:17 +0000

    Remove commented out code

--- tests/test-create-texture.cpp
+++ tests/test-create-texture.cpp
@@ -11,8 +11,6 @@ GPU_TEST_CASE("texturetest-create", ALL)
 {
     TextureTestOptions options(device, TextureInitMode::Random);
     options.addVariants(TTShape::All, TTArray::Both, TTMip::Both, TTMS::Both);
-    // options.addVariants(TTShape::All, TTArray::Off, TTMip::Off, TTMS::On);
-
 
     runTextureTest(
         options,
