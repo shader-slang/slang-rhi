c71ab0a4aaed16afaeaddd48ac12de17bf39f9fa
Chris Cummings <ccummings@nvidia.com>
2025-03-18 17:15:31 +0000

    Check for null queue

--- src/vulkan/vk-device.cpp
+++ src/vulkan/vk-device.cpp
@@ -36,7 +36,10 @@ DeviceImpl::DeviceImpl() {}
 DeviceImpl::~DeviceImpl()
 {
     // Wait for all commands to finish and retire any active command buffers.
-    m_queue->waitOnHost();
+    if (m_queue)
+    {
+        m_queue->waitOnHost();
+    }
 
     // Check the device queue is valid else, we can't wait on it..
     if (m_deviceQueue.isValid())
