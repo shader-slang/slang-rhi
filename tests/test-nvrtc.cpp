9f12e19d33607a8d16a434fc1778042c5910b2e5
Simon Kallweit <skallweit@nvidia.com>
2025-03-19 23:48:36 +0100

    add nvrtc wrapper

---
+++ tests/test-nvrtc.cpp
@@ -0,0 +1,33 @@
+#include "testing.h"
+
+#if SLANG_RHI_ENABLE_CUDA
+
+#include "cuda/cuda-nvrtc.h"
+
+using namespace rhi;
+using namespace rhi::testing;
+using namespace rhi::cuda;
+
+TEST_CASE("nvrtc")
+{
+    NVRTC nvrtc;
+    if (nvrtc.init() != SLANG_OK)
+    {
+        return;
+    }
+
+    const char* source = R"(
+        #include <cuda_runtime.h>
+        extern "C" __global__ void dummyKernel() {
+            int idx = threadIdx.x;
+        }
+    )";
+
+    NVRTC::CompileResult compileResult;
+    Result result = nvrtc.compilePTX(source, compileResult);
+    CHECK(result == SLANG_OK);
+    CHECK(compileResult.result == NVRTC_SUCCESS);
+    CHECK(compileResult.ptx.size() > 0);
+}
+
+#endif // SLANG_RHI_ENABLE_CUDA
