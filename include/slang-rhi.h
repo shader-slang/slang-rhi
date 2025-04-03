2666879d993604385fbf1daa8f02c2ac9cfeff19
ccummingsNV <ccummings@nvidia.com>
2025-04-03 11:51:36 +0100

    Expose shader cache stats (#291)
    * Expose shader cache stats
    
    * Add shader cache stats to debug device
    
    * Add missing 'override'

--- include/slang-rhi.h
+++ include/slang-rhi.h
@@ -2817,6 +2817,9 @@ public:
 
     virtual SLANG_NO_THROW Result SLANG_MCALL
     convertCooperativeVectorMatrix(const ConvertCooperativeVectorMatrixDesc* descs, uint32_t descCount) = 0;
+
+    virtual SLANG_NO_THROW Result SLANG_MCALL
+    getShaderCacheStats(size_t* outCacheHitCount, size_t* outCacheMissCount, size_t* outCacheSize) = 0;
 };
 
 class ITaskScheduler : public ISlangUnknown
