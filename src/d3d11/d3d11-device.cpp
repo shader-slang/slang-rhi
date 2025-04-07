50f19b3a1141d92fde62552983c6c8ff37ed0e77
Aidan Foster <aidanf@nvidia.com>
2025-04-07 15:07:02 -0700

    Remove trailing whitespace from doublePrecisionFeature

--- src/d3d11/d3d11-device.cpp
+++ src/d3d11/d3d11-device.cpp
@@ -257,7 +257,7 @@ Result DeviceImpl::initialize(const DeviceDesc& desc)
         D3D11_FEATURE_DATA_DOUBLES doublePrecisionFeature = {};
         if (SUCCEEDED(m_device->CheckFeatureSupport(
                 D3D11_FEATURE_DOUBLES,
-                &doublePrecisionFeature, 
+                &doublePrecisionFeature,
                 sizeof(doublePrecisionFeature))) &&
             doublePrecisionFeature.DoublePrecisionFloatShaderOps)
         {
