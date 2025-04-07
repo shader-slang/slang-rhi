e582c95aa5cdec8d3c4628714f357ae1633bf870
Aidan Foster <aidanf@nvidia.com>
2025-04-07 15:05:46 -0700

    Fix formatting of d3d11 double support check

--- src/d3d11/d3d11-device.cpp
+++ src/d3d11/d3d11-device.cpp
@@ -255,8 +255,11 @@ Result DeviceImpl::initialize(const DeviceDesc& desc)
     // Check double precision support
     {
         D3D11_FEATURE_DATA_DOUBLES doublePrecisionFeature = {};
-        if (SUCCEEDED(m_device->CheckFeatureSupport(D3D11_FEATURE_DOUBLES, &doublePrecisionFeature, sizeof(doublePrecisionFeature))) 
-            && doublePrecisionFeature.DoublePrecisionFloatShaderOps)
+        if (SUCCEEDED(m_device->CheckFeatureSupport(
+                D3D11_FEATURE_DOUBLES,
+                &doublePrecisionFeature, 
+                sizeof(doublePrecisionFeature))) &&
+            doublePrecisionFeature.DoublePrecisionFloatShaderOps)
         {
             m_features.add("double");
         }
