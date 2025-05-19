46cd9315b4250206e85c718c40c5bcf835175681
Simon Kallweit <skallweit@nvidia.com>
2025-05-19 15:29:09 +0200

    wip

--- tests/test-formats.cpp
+++ tests/test-formats.cpp
@@ -658,7 +658,7 @@ struct TestFormats
 
 // skip CPU: Vector types not implemented
 // skip CUDA: GetDimensions not implemented
-GPU_TEST_CASE("formats", D3D11 | D3D12 | Vulkan | Metal | WGPU)
+GPU_TEST_CASE("formats", D3D11 | D3D12 | Vulkan | Metal | WGPU | CUDA)
 {
     TestFormats test;
     test.init(device);
