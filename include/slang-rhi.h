3406625d563aaa4595fb7e031e14e3716e69fbf0
Simon Kallweit <skallweit@nvidia.com>
2025-06-04 07:53:39 -0700

    Revert "do not ref-count resources passed in Binding (#377)"
    This reverts commit 89722ca6cb89efadd74913096ee5357d63732965.

--- include/slang-rhi.h
+++ include/slang-rhi.h
@@ -1458,11 +1458,8 @@ enum class BindingType
 struct Binding
 {
     BindingType type = BindingType::Undefined;
-    // We don't use ComPtr<IResource> here because we want to avoid ref-counting overhead.
-    // When using the Binding constructors taking ComPtr<> parameters, the temporary ComPtr<> will make sure that
-    // the resource is kept alive until the Binding is destroyed (which is after setting it on a shader object).
-    IResource* resource;
-    IResource* resource2;
+    ComPtr<IResource> resource;
+    ComPtr<IResource> resource2;
     union
     {
         BufferRange bufferRange;
@@ -1477,8 +1474,8 @@ struct Binding
     Binding(IBuffer* buffer, IBuffer* counter, const BufferRange& range = kEntireBuffer) : type(BindingType::BufferWithCounter), resource(buffer), resource2(counter), bufferRange(range) {}
     Binding(const ComPtr<IBuffer>& buffer, const ComPtr<IBuffer>& counter, const BufferRange& range = kEntireBuffer) : type(BindingType::BufferWithCounter), resource(buffer), resource2(counter), bufferRange(range) {}
 
-    Binding(ITexture* texture) : type(BindingType::Texture), resource(texture->getDefaultView().get()) {}
-    Binding(const ComPtr<ITexture>& texture) : type(BindingType::Texture), resource(texture->getDefaultView().get()) {}
+    Binding(ITexture* texture) : type(BindingType::Texture), resource(texture->getDefaultView()) {}
+    Binding(const ComPtr<ITexture>& texture) : type(BindingType::Texture), resource(texture->getDefaultView()) {}
 
     Binding(ITextureView* textureView) : type(BindingType::Texture), resource(textureView) {}
     Binding(const ComPtr<ITextureView>& textureView) : type(BindingType::Texture), resource(textureView) {}
@@ -1486,8 +1483,8 @@ struct Binding
     Binding(ISampler* sampler) : type(BindingType::Sampler) , resource(sampler) {}
     Binding(const ComPtr<ISampler>& sampler) : type(BindingType::Sampler) , resource(sampler) {}
 
-    Binding(ITexture* texture, ISampler* sampler) : type(BindingType::CombinedTextureSampler), resource(texture->getDefaultView().get()), resource2(sampler) {}
-    Binding(const ComPtr<ITexture>& texture, const ComPtr<ISampler>& sampler) : type(BindingType::CombinedTextureSampler), resource(texture->getDefaultView().get()), resource2(sampler) {}
+    Binding(ITexture* texture, ISampler* sampler) : type(BindingType::CombinedTextureSampler), resource(texture->getDefaultView()), resource2(sampler) {}
+    Binding(const ComPtr<ITexture>& texture, const ComPtr<ISampler>& sampler) : type(BindingType::CombinedTextureSampler), resource(texture->getDefaultView()), resource2(sampler) {}
 
     Binding(ITextureView* textureView, ISampler* sampler) : type(BindingType::CombinedTextureSampler) , resource(textureView), resource2(sampler) {}
     Binding(const ComPtr<ITextureView>& textureView, const ComPtr<ISampler>& sampler) : type(BindingType::CombinedTextureSampler) , resource(textureView), resource2(sampler) {}
