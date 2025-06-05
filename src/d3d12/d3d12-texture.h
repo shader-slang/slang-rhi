3dec4003db6ca5e2c5fb4191598a137086f9c2a2
Tomas Davidovic <tdavidovic@nvidia.com>
2025-06-05 06:23:00 +0200

    More meaningful default

--- src/d3d12/d3d12-texture.h
+++ src/d3d12/d3d12-texture.h
@@ -12,7 +12,7 @@ public:
 
     D3D12Resource m_resource;
     DXGI_FORMAT m_format = DXGI_FORMAT_UNKNOWN;
-    bool m_isTypeless = false;
+    bool m_isTypeless = true;
     D3D12_RESOURCE_STATES m_defaultState = D3D12_RESOURCE_STATE_COMMON;
 
     virtual SLANG_NO_THROW Result SLANG_MCALL getNativeHandle(NativeHandle* outHandle) override;
