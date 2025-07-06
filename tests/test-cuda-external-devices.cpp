d5f4375c9ca88c4acf7a1959dff74141e8574cec
Chris Cummings <ccummings@nvidia.com>
2025-07-06 11:08:01 +0100

    Bind pointers test + start on external device test

---
+++ tests/test-cuda-external-devices.cpp
@@ -0,0 +1,13 @@
+#include "testing.h"
+#include <slang-rhi.h>
+#include "core/smart-pointer.h"
+#include "rhi-shared.h"
+#include "device.h"
+
+using namespace rhi;
+using namespace rhi::testing;
+
+GPU_TEST_CASE("device-lifetime", CUDA)
+{
+ 
+}
