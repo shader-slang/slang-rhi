c1b0026a37ae75b23669254a7056b9957139e8d7
Chris Cummings <ccummings@nvidia.com>
2025-07-14 14:20:36 +0100

    Disable user config

--- include/slang-user-config.h
+++ include/slang-user-config.h
@@ -1,4 +1,4 @@
 #pragma once
 
 // Enable this to trigger a debug break whenever a SLANG_RETURN_ON_FAIL triggers
-#define SLANG_HANDLE_RESULT_FAIL(err) __debugbreak()
+// #define SLANG_HANDLE_RESULT_FAIL(err) __debugbreak()
