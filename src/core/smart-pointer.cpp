d96ec2df4daf153377305e32866c34b3a9879bec
Simon Kallweit <64953474+skallweitNV@users.noreply.github.com>
2025-08-15 11:32:05 +0200

    Dump live objects with class names if RTTI data is available (#485)
    * Dump live objects with class names if RTTI data is available
    
    * Improve live object reporting

--- src/core/smart-pointer.cpp
+++ src/core/smart-pointer.cpp
@@ -2,6 +2,20 @@
 
 #include <cstdio>
 
+#if SLANG_VC
+#if defined(_CPPRTTI)
+#define RTTI_ENABLED
+#endif
+#elif SLANG_CLANG
+#if __has_feature(cxx_rtti)
+#define RTTI_ENABLED
+#endif
+#elif SLANG_GCC
+#if defined(__GXX_RTTI)
+#define RTTI_ENABLED
+#endif
+#endif
+
 namespace rhi {
 
 #if SLANG_RHI_ENABLE_REF_OBJECT_TRACKING
@@ -12,7 +26,20 @@ void RefObjectTracker::reportLiveObjects()
         printf("Found %zu live RHI objects!\n", objects.size());
         for (auto obj : objects)
         {
-            printf("Live object: %p\n", static_cast<void*>(obj));
+            uint64_t referenceCount = obj->getReferenceCount();
+            uint64_t internalReferenceCount = obj->getInternalReferenceCount();
+#ifdef RTTI_ENABLED
+            const char* typeName = typeid(*obj).name();
+#else
+            const char* typeName = "unknown";
+#endif
+            printf(
+                "Live object: 0x%" PRIXPTR " referenceCount=%llu internalReferenceCount=%llu type=\"%s\"\n",
+                reinterpret_cast<uintptr_t>(obj),
+                referenceCount,
+                internalReferenceCount,
+                typeName
+            );
         }
     }
 }
