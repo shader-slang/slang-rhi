a7aaba3a815a97f0f3efed0e1a868b62f6c54169
Simon Kallweit <64953474+skallweitNV@users.noreply.github.com>
2025-09-04 15:01:22 +0200

    GPU tests implementation (#511)
    * Register one test case for each device type of a GPU test
    
    * Only register GPU tests for platform supported device types
    
    * Add SKIP macro
    
    * Improve test reporting
    
    * Update copilot instructions

--- tests/testing.h
+++ tests/testing.h
@@ -285,23 +285,46 @@ auto makeArray(Args... args)
     return std::array<T, sizeof...(Args)>{static_cast<T>(args)...};
 }
 
-#define ALL_DEVICE_TYPES                                                                                               \
-    {                                                                                                                  \
-        rhi::DeviceType::D3D11,                                                                                        \
-        rhi::DeviceType::D3D12,                                                                                        \
-        rhi::DeviceType::Vulkan,                                                                                       \
-        rhi::DeviceType::Metal,                                                                                        \
-        rhi::DeviceType::CPU,                                                                                          \
-        rhi::DeviceType::CUDA,                                                                                         \
-        rhi::DeviceType::WGPU,                                                                                         \
+static constexpr DeviceType kPlatformDeviceTypes[] = {
+#if SLANG_WINDOWS_FAMILY
+    rhi::DeviceType::D3D11,
+    rhi::DeviceType::D3D12,
+    rhi::DeviceType::Vulkan,
+    rhi::DeviceType::CPU,
+    rhi::DeviceType::CUDA,
+    rhi::DeviceType::WGPU,
+#elif SLANG_LINUX_FAMILY
+    rhi::DeviceType::Vulkan,
+    rhi::DeviceType::CPU,
+    rhi::DeviceType::CUDA,
+    rhi::DeviceType::WGPU,
+#elif SLANG_APPLE_FAMILY
+    rhi::DeviceType::Vulkan,
+    rhi::DeviceType::Metal,
+    rhi::DeviceType::CPU,
+    rhi::DeviceType::CUDA,
+    rhi::DeviceType::WGPU,
+#endif
+};
+
+inline bool isPlatformDeviceType(DeviceType deviceType)
+{
+    for (DeviceType platformDeviceType : kPlatformDeviceTypes)
+    {
+        if (platformDeviceType == deviceType)
+        {
+            return true;
+        }
     }
+    return false;
+}
 
 using GpuTestFunc = void (*)(GpuTestContext*, ComPtr<IDevice>);
 
-void runGpuTestFunc(GpuTestFunc func, int testFlags);
-
 enum GpuTestFlags
 {
+    None = 0,
+
     // Device type flags
     D3D11 = (1 << (int)DeviceType::D3D11),
     D3D12 = (1 << (int)DeviceType::D3D12),
@@ -317,24 +340,54 @@ enum GpuTestFlags
     DontCacheDevice = (1 << 11),  // Do not use cached devices (create a new device for this test case)
 };
 
+struct GpuTestInfo
+{
+    GpuTestFunc func;
+    DeviceType deviceType;
+    GpuTestFlags flags;
+};
+static_assert(std::is_pod_v<GpuTestInfo>, "GpuTestInfo must be POD");
+
+int registerGpuTest(const char* name, GpuTestFunc func, GpuTestFlags flags, const char* file, int line);
+
+void reportSkip(const doctest::detail::TestCase* tc, const char* reason);
+const char* getSkipMessage(const doctest::TestCaseData* tc);
+
 } // namespace rhi::testing
 
-#define GPU_TEST_CASE_IMPL(name, func, testFlags)                                                                      \
+#define GPU_TEST_CASE_IMPL(name, func, flags)                                                                          \
     static void func(::rhi::testing::GpuTestContext* ctx, ::ComPtr<::rhi::IDevice> device);                            \
-    TEST_CASE(name)                                                                                                    \
-    {                                                                                                                  \
-        ::rhi::testing::runGpuTestFunc(func, testFlags);                                                               \
-    }                                                                                                                  \
+    DOCTEST_GLOBAL_NO_WARNINGS(                                                                                        \
+        DOCTEST_ANONYMOUS(DOCTEST_ANON_VAR_),                                                                          \
+        ::rhi::testing::registerGpuTest(                                                                               \
+            name,                                                                                                      \
+            func,                                                                                                      \
+            static_cast<::rhi::testing::GpuTestFlags>(flags),                                                          \
+            __FILE__,                                                                                                  \
+            __LINE__                                                                                                   \
+        )                                                                                                              \
+    )                                                                                                                  \
     static void func(::rhi::testing::GpuTestContext* ctx, ::ComPtr<::rhi::IDevice> device)
 
-#define GPU_TEST_CASE(name, testFlags) GPU_TEST_CASE_IMPL(name, DOCTEST_ANONYMOUS(GPU_TEST_ANONYMOUS_), testFlags)
+// Register a GPU test case.
+// This will register one test case for each device type specified in the flags.
+// Each test will be named <name>.<deviceType> where <deviceType> is the string representation of the device type.
+// The GPU test function has the following signature: void func(GpuTestContext* ctx, ComPtr<IDevice> device)
+// In addition to the device flags, the following flags can be used:
+// - GpuTestFlag::DontCreateDevice: Do not create a device (device argument is nullptr)
+// - GpuTestFlag::DontCacheDevice: Do not use cached devices (create a new device for this test case)
+#define GPU_TEST_CASE(name, flags) GPU_TEST_CASE_IMPL(name, DOCTEST_ANONYMOUS(GPU_TEST_ANONYMOUS_), flags)
 
 #define CHECK_CALL(x) CHECK(!SLANG_FAILED(x))
 #define REQUIRE_CALL(x) REQUIRE(!SLANG_FAILED(x))
 
+// doctest does not support skipping tests at runtime.
+// We add this functionality using this SKIP macro which should only be called in the main scope of the test function.
+// The `reason` argument MUST be a string literal.
 #define SKIP(reason)                                                                                                   \
     do                                                                                                                 \
     {                                                                                                                  \
+        ::rhi::testing::reportSkip(::doctest::getContextOptions()->currentTest, "" reason);                            \
         return;                                                                                                        \
     }                                                                                                                  \
     while (0)
