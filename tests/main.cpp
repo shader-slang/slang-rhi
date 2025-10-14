3307561d59906e96e3f8203f8bc828f6216ac084
Simon Kallweit <skallweit@nvidia.com>
2025-10-14 17:51:14 +0200

    Allow setting OptiX version used for running tests

--- tests/main.cpp
+++ tests/main.cpp
@@ -75,6 +75,7 @@ int main(int argc, const char** argv)
                 }
             }
         }
+        doctest::parseIntOption(argc, argv, "optix-version", doctest::option_int, options.optixVersion);
     }
 
     int result = 1;
