3a1ccf89e646625b3f8e5cfd31317be8c29eed00
Kincaid Savoie <ksavoie@nvidia.com>
2025-10-31 15:57:23 -0600

    restore OptiX version check in ray-tracing-hitobject-make-hit

--- tests/ray-tracing/test-ray-tracing-hitobject-intrinsics.cpp
+++ tests/ray-tracing/test-ray-tracing-hitobject-intrinsics.cpp
@@ -421,6 +421,10 @@ GPU_TEST_CASE("ray-tracing-hitobject-make-hit", ALL | DontCreateDevice)
     if (!device->hasFeature(Feature::ShaderExecutionReordering))
         SKIP("shader execution reordering not supported");
 
+    // Disabled under pre OptiX 9.0 due to https://github.com/shader-slang/slang/issues/8723
+    if (device->getDeviceType() == DeviceType::CUDA && device->getInfo().optixVersion < 90000)
+        SKIP("MakeHit not functional with specified OptiX version");
+
     RayTracingSingleTriangleTest test;
     test.init(device);
     test.createResultBuffer(sizeof(TestResult));
