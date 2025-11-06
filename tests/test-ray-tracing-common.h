7203417c51baa52388d0e704359d169d1558c75c
Kincaid Savoie <ksavoie@nvidia.com>
2025-11-06 15:25:52 -0700

    Delete ray-tracing-common.cpp and inline launchPipeline
    launchPipeline was the only function in ray-tracing-common.cpp. Inline
    the function into ray-tracing-common.h and delete the basically empty
    cpp file.

--- tests/test-ray-tracing-common.h
+++ tests/test-ray-tracing-common.h
@@ -716,12 +716,25 @@ struct RayTracingTestPipeline
     }
 };
 
-void launchPipeline(
+inline void launchPipeline(
     ICommandQueue* queue,
     IRayTracingPipeline* pipeline,
     IShaderTable* shaderTable,
     IBuffer* resultBuffer,
     IAccelerationStructure* tlas
-);
+)
+{
+    auto commandEncoder = queue->createCommandEncoder();
 
+    auto passEncoder = commandEncoder->beginRayTracingPass();
+    auto rootObject = passEncoder->bindPipeline(pipeline, shaderTable);
+    auto cursor = ShaderCursor(rootObject);
+    cursor["resultBuffer"].setBinding(resultBuffer);
+    cursor["sceneBVH"].setBinding(tlas);
+    passEncoder->dispatchRays(0, 1, 1, 1);
+    passEncoder->end();
+
+    queue->submit(commandEncoder->finish());
+    queue->waitOnHost();
+}
 } // namespace rhi::testing
