6eb0590adf4693784500ba825ba94a92844579f3
Simon Kallweit <skallweit@nvidia.com>
2026-02-09 23:27:22 +0100

    reverseMap without heap allocation

--- src/core/reverse-map.h
+++ src/core/reverse-map.h
@@ -1,30 +1,32 @@
 #pragma once
 
-#include <unordered_map>
+#include <array>
+#include <utility>
 
 namespace rhi {
 
 /// Given a mapping function, create a reverse map from To to From.
 /// The mapping function func must be bijective.
 /// The reverse map will return defaultValue if the value is not found.
-template<typename From, typename To, typename Func>
-auto reverseMap(Func func, From min, From max, From defaultValue = From(0))
+template<typename From, typename To, From min, From max, typename Func>
+auto reverseMap(Func func, From defaultValue = From(0))
 {
-    static std::unordered_map<To, From> reverseMap = [&]()
+    constexpr size_t size = size_t(max) - size_t(min) + 1;
+    static std::array<std::pair<To, From>, size> data = [&]()
     {
-        std::unordered_map<To, From> map;
-        for (int i = int(min); i <= int(max); i++)
+        std::array<std::pair<To, From>, size> arr{};
+        for (size_t i = 0; i < size; i++)
         {
-            const From fromI = From(i);
-            const To key = func(fromI); // Fixed MSVC warning C4709: comma operator within a subscript expression
-            map[key] = fromI;
+            arr[i] = {func(From(int(min) + int(i))), From(int(min) + int(i))};
         }
-        return map;
+        return arr;
     }();
     return [defaultValue](To value) -> From
     {
-        auto it = reverseMap.find(value);
-        return it == reverseMap.end() ? defaultValue : it->second;
+        for (const auto& [k, v] : data)
+            if (k == value)
+                return v;
+        return defaultValue;
     };
 }
 
