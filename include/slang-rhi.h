5aa3ae2e6ff34f9352cd5020ea98f452f3195e18
kaizhangNV <kazhang@nvidia.com>
2026-02-12 08:46:48 -0800

    address comment

--- include/slang-rhi.h
+++ include/slang-rhi.h
@@ -164,7 +164,8 @@ enum class DeviceType
     x(ShaderResourceMinLod,                     "shader-resource-min-lod"                       ) \
     /* Metal specific features */                                                                 \
     x(ArgumentBufferTier2,                      "argument-buffer-tier-2"                        ) \
-    x(AtomicBfloat16,                           "atomic-bfloat16"                               )
+    /* CUDA specific features */                                                                  \
+    x(AtomicBfloat16,                           "atomic-bfloat16"                               ) \
 // clang-format on
 
 #define SLANG_RHI_FEATURE_X(e, _) e,
