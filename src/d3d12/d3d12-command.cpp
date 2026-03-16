befcecffdaae80c8f274f1cb8d89ec3292a14e72
Simon Kallweit <simon.kallweit@gmail.com>
2026-03-16 23:57:09 +0100

    Do not rely on strings

--- src/d3d12/d3d12-command.cpp
+++ src/d3d12/d3d12-command.cpp
@@ -177,7 +177,7 @@ Result CommandRecorder::record(CommandBufferImpl* commandBuffer)
     return SLANG_OK;
 }
 
-#define NOT_SUPPORTED(x) m_device->printWarning(S_CommandEncoder_##x " command is not supported!")
+#define NOT_SUPPORTED(interface, method) m_device->printWarning(#interface "::" #method " is not supported!")
 
 void CommandRecorder::cmdCopyBuffer(const commands::CopyBuffer& cmd)
 {
