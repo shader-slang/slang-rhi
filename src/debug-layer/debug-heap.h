80241c9f56fa658707ab457749cb156e1fba26a7
Simon Kallweit <skallweit@nvidia.com>
2026-03-17 16:10:35 +0100

    Improve DebugHeap

--- src/debug-layer/debug-heap.h
+++ src/debug-layer/debug-heap.h
@@ -8,20 +8,16 @@ class DebugHeap : public DebugObject<IHeap>
 {
 public:
     SLANG_COM_OBJECT_IUNKNOWN_ALL;
+    IHeap* getInterface(const Guid& guid);
 
     SLANG_RHI_DEBUG_OBJECT_CONSTRUCTOR(DebugHeap);
 
-    IHeap* getInterface(const Guid& guid);
-
 public:
-    virtual SLANG_NO_THROW Result SLANG_MCALL allocate(const HeapAllocDesc& desc, HeapAlloc* allocation) override;
-
+    // IHeap implementation
+    virtual SLANG_NO_THROW Result SLANG_MCALL allocate(const HeapAllocDesc& desc, HeapAlloc* outAllocation) override;
     virtual SLANG_NO_THROW Result SLANG_MCALL free(HeapAlloc allocation) override;
-
     virtual SLANG_NO_THROW Result SLANG_MCALL report(HeapReport* outReport) override;
-
     virtual SLANG_NO_THROW Result SLANG_MCALL flush() override;
-
     virtual SLANG_NO_THROW Result SLANG_MCALL removeEmptyPages() override;
 };
 
