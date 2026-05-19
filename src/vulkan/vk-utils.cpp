68416bef6bc6a4dd685e6f24b958d1cc06959f7e
Jussi Vepsalainen (NVIDIA) <161336110+jvepsalainen-nv@users.noreply.github.com>
2026-05-19 14:00:05 +0300

    Tighten Vulkan barrier stage masks

--- src/vulkan/vk-utils.cpp
+++ src/vulkan/vk-utils.cpp
@@ -330,14 +330,13 @@ VkAccessFlagBits calcAccessFlags(ResourceState state)
 
 static VkPipelineStageFlags getSupportedShaderStageFlags(const VulkanApi& api)
 {
-    VkPipelineStageFlags result = VK_PIPELINE_STAGE_VERTEX_SHADER_BIT |
-                                  VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT |
+    VkPipelineStageFlags result = VK_PIPELINE_STAGE_VERTEX_SHADER_BIT | VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT |
                                   VK_PIPELINE_STAGE_COMPUTE_SHADER_BIT;
 
     if (api.m_deviceFeatures.tessellationShader)
     {
-        result |= VK_PIPELINE_STAGE_TESSELLATION_CONTROL_SHADER_BIT |
-                  VK_PIPELINE_STAGE_TESSELLATION_EVALUATION_SHADER_BIT;
+        result |=
+            VK_PIPELINE_STAGE_TESSELLATION_CONTROL_SHADER_BIT | VK_PIPELINE_STAGE_TESSELLATION_EVALUATION_SHADER_BIT;
     }
 
     if (api.m_deviceFeatures.geometryShader)
@@ -345,6 +344,11 @@ static VkPipelineStageFlags getSupportedShaderStageFlags(const VulkanApi& api)
 
     if (api.m_extendedFeatures.rayTracingPipelineFeatures.rayTracingPipeline)
         result |= VK_PIPELINE_STAGE_RAY_TRACING_SHADER_BIT_KHR;
+
+    if (api.m_extendedFeatures.meshShaderFeatures.taskShader)
+        result |= VK_PIPELINE_STAGE_TASK_SHADER_BIT_EXT;
+    if (api.m_extendedFeatures.meshShaderFeatures.meshShader)
+        result |= VK_PIPELINE_STAGE_MESH_SHADER_BIT_EXT;
 
     return result;
 }
@@ -384,12 +388,7 @@ VkPipelineStageFlags calcPipelineStageFlags(const VulkanApi& api, ResourceState 
     case ResourceState::General:
         return VkPipelineStageFlagBits(VK_PIPELINE_STAGE_ALL_COMMANDS_BIT);
     case ResourceState::AccelerationStructureRead:
-    {
-        VkPipelineStageFlags result = VK_PIPELINE_STAGE_COMPUTE_SHADER_BIT;
-        if (api.m_extendedFeatures.rayTracingPipelineFeatures.rayTracingPipeline)
-            result |= VK_PIPELINE_STAGE_RAY_TRACING_SHADER_BIT_KHR;
-        return result;
-    }
+        return getSupportedShaderStageFlags(api) | VK_PIPELINE_STAGE_ACCELERATION_STRUCTURE_BUILD_BIT_KHR;
     case ResourceState::AccelerationStructureWrite:
         return VK_PIPELINE_STAGE_ACCELERATION_STRUCTURE_BUILD_BIT_KHR;
     case ResourceState::AccelerationStructureBuildInput:
